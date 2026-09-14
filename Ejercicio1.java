65e33a24798ee977d5d2afdc131d49c66d86f1ce
samueleduardo1061-cpu <samueleduardo1061@gmail.com>
2026-09-13 21:58:46 -0500

    pollo

---
+++ Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+        int[] primos = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+        
+        System.out.println("Los primeros 10 números primos son:");
+        for (int primo : primos) {
+            System.out.print(primo); 
+            
+        }
+    }
+}
